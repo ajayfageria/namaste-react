3d234ab915c89bcfa2e828b9ec86146b7f4c5cf1
DishaSharma-dev <56217410+DishaSharma-dev@users.noreply.github.com>
2023-04-11 20:38:18 +0530

    Ep05 - filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,12 @@
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../../utils/mockdata";
 const Body = () => {
+  const filterTopResta = () => {
+    console.log('filter')
+  }
   return (
     <div>
-      <div className="search">Search</div>
+      <div className="filter"><button onClick={filterTopResta}> Top Reated Restaurants</button></div>
       <div className="res-container">
         {resList.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
